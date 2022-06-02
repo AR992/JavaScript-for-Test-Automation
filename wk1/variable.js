3e6ed3e0c08c9cd2f3a769f3a35fa6ca1a949561
Ajdin Redzepovac <aredzepovac@fingercheck.com>
2022-06-03 00:08:09 +0200

    learn comments and varible of JS

---
+++ wk1/variable.js
@@ -0,0 +1,17 @@
+
+/**
+* Variables srules:
+1. Variable name should start with a letter, or underscore (_), or dollar sign ($)
+2. System reserved keywords can't be used: if, else, throw, break
+3. Varaible names are case-sensitive
+4. Can't have same name for other types (function name, object name)
+5. Multiple variables can be declated in single line (separated by comma)
+6. When you just declare a varaible, the data type will be undefined
+ */
+
+let greeting = "Hello World!", newVariable = "This is string"
+console.log(greeting, newVariable)
+
+
+let newName
+console.log(newName)  //here will be undefind
